0d23eb749933e8b652b18812daba176b5df0df11
Andrey Petkevich(Akvelon) <v-anpetk@microsoft.com>
2018-06-25 16:30:25 -0700

    fix url to backend

--- frontend/src/components/Home.vue
+++ frontend/src/components/Home.vue
@@ -26,7 +26,8 @@ export default {
       this.randomNumber = this.getRandomInt(1, 100);
     },
     getRandomFromBackend () {
-      const path = `http://localhost:5000/api/random`;
+      // const path = `http://localhost:5000/api/random`;
+      const path = `${window.location.protocol}//${window.location.hostname}:5000/api/random`;
       axios.get(path)
         .then(response => {
           this.randomNumber = response.data.randomNumber;
